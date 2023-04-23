16b6a49afa0d0af25f4783f802cb40befd3d8bea
franprog1 <130921772+franprog1@users.noreply.github.com>
2023-04-23 19:35:23 -0300

    Add files via upload

---
+++ FColorestp2/FColorestp2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace FColorestp2
+{
+    partial class FColores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BAmarillo = new System.Windows.Forms.Button();
+            this.BRojo = new System.Windows.Forms.Button();
+            this.Bazul = new System.Windows.Forms.Button();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.lAmarillo = new System.Windows.Forms.Label();
+            this.lRojo = new System.Windows.Forms.Label();
+            this.lAzul = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BAmarillo
+            // 
+            this.BAmarillo.Location = new System.Drawing.Point(140, 68);
+            this.BAmarillo.Name = "BAmarillo";
+            this.BAmarillo.Size = new System.Drawing.Size(148, 37);
+            this.BAmarillo.TabIndex = 0;
+            this.BAmarillo.Text = "&Amarillo";
+            this.BAmarillo.UseVisualStyleBackColor = true;
+            this.BAmarillo.Click += new System.EventHandler(this.BAmarillo_Click);
+            // 
+            // BRojo
+            // 
+            this.BRojo.Location = new System.Drawing.Point(140, 111);
+            this.BRojo.Name = "BRojo";
+            this.BRojo.Size = new System.Drawing.Size(148, 37);
+            this.BRojo.TabIndex = 1;
+            this.BRojo.Text = "&Rojo";
+            this.BRojo.UseVisualStyleBackColor = true;
+            this.BRojo.Click += new System.EventHandler(this.BRojo_Click);
+            // 
+            // Bazul
+            // 
+            this.Bazul.Location = new System.Drawing.Point(140, 154);
+            this.Bazul.Name = "Bazul";
+            this.Bazul.Size = new System.Drawing.Size(148, 37);
+            this.Bazul.TabIndex = 2;
+            this.Bazul.Text = "A&zul";
+            this.Bazul.UseVisualStyleBackColor = true;
+            this.Bazul.Click += new System.EventHandler(this.Bazul_Click);
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.Location = new System.Drawing.Point(310, 258);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(95, 36);
+            this.bCerrar.TabIndex = 3;
+            this.bCerrar.Text = "Cerrar";
+            this.bCerrar.UseVisualStyleBackColor = true;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // lAmarillo
+            // 
+            this.lAmarillo.AutoSize = true;
+            this.lAmarillo.Location = new System.Drawing.Point(310, 80);
+            this.lAmarillo.Name = "lAmarillo";
+            this.lAmarillo.Size = new System.Drawing.Size(13, 13);
+            this.lAmarillo.TabIndex = 4;
+            this.lAmarillo.Text = "0";
+            this.lAmarillo.Visible = false;
+            // 
+            // lRojo
+            // 
+            this.lRojo.AutoSize = true;
+            this.lRojo.Location = new System.Drawing.Point(310, 123);
+            this.lRojo.Name = "lRojo";
+            this.lRojo.Size = new System.Drawing.Size(13, 13);
+            this.lRojo.TabIndex = 5;
+            this.lRojo.Text = "0";
+            this.lRojo.Visible = false;
+            // 
+            // lAzul
+            // 
+            this.lAzul.AutoSize = true;
+            this.lAzul.Location = new System.Drawing.Point(310, 166);
+            this.lAzul.Name = "lAzul";
+            this.lAzul.Size = new System.Drawing.Size(13, 13);
+            this.lAzul.TabIndex = 6;
+            this.lAzul.Text = "0";
+            this.lAzul.Visible = false;
+            // 
+            // FColores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 311);
+            this.Controls.Add(this.lAzul);
+            this.Controls.Add(this.lRojo);
+            this.Controls.Add(this.lAmarillo);
+            this.Controls.Add(this.bCerrar);
+            this.Controls.Add(this.Bazul);
+            this.Controls.Add(this.BRojo);
+            this.Controls.Add(this.BAmarillo);
+            this.Name = "FColores";
+            this.Text = "Mezcla de Colores";
+            this.Load += new System.EventHandler(this.FColores_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BAmarillo;
+        private System.Windows.Forms.Button BRojo;
+        private System.Windows.Forms.Button Bazul;
+        private System.Windows.Forms.Button bCerrar;
+        private System.Windows.Forms.Label lAmarillo;
+        private System.Windows.Forms.Label lRojo;
+        private System.Windows.Forms.Label lAzul;
+    }
+}
+
---
+++ FColorestp2/FColorestp2/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FColorestp2
+{
+    public partial class FColores : Form
+    {
+        int amarilloRequerido;
+        int rojoRequerido;
+        int azulRequerido;
+        int Primero, Segundo, Tercero ;
+        public FColores()
+        {
+            amarilloRequerido = 0;
+            rojoRequerido = 0;
+            azulRequerido = 0;
+            InitializeComponent();
+        }
+
+        private void FColores_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BAmarillo_Click(object sender, EventArgs e)
+        {
+            Color colorActual = BackColor;
+            if (colorActual == Color.Orange)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if (colorActual == Color.Blue)
+            {
+                this.BackColor = Color.Green;
+            }
+            else if (colorActual == Color.Green)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if (colorActual == Color.Purple)
+            {
+                this.BackColor = Color.Black;
+            }
+
+            else if (colorActual == Color.Red)
+            {
+                this.BackColor = Color.Orange;
+            }
+            else
+            {
+                this.BackColor = Color.Yellow;
+            }
+            amarilloRequerido = int.Parse(lAmarillo.Text);
+            amarilloRequerido++;
+            lAmarillo.Text = amarilloRequerido.ToString();
+        }
+
+        private void BRojo_Click(object sender, EventArgs e)
+        {
+            Color colorActual = BackColor;
+            if (colorActual == Color.Orange)
+            {
+                this.BackColor = Color.Red;
+            }
+            else if (colorActual == Color.Purple)
+            {
+                this.BackColor = Color.Red;
+            }
+            else if (colorActual == Color.Yellow)
+            {
+                this.BackColor = Color.Orange;
+            }
+            else if (colorActual == Color.Green)
+            {
+                this.BackColor = Color.Black;
+            }
+            else if (colorActual == Color.Blue)
+            {
+                this.BackColor = Color.Purple;
+            }
+            else
+            {
+                this.BackColor = Color.Red;
+            }
+            rojoRequerido = int.Parse(lRojo.Text);
+            rojoRequerido++;
+            lRojo.Text = rojoRequerido.ToString();
+          
+        }
+
+        private void Bazul_Click(object sender, EventArgs e)
+        {
+            Color colorActual = BackColor;
+            if (colorActual == Color.Yellow)
+            {
+                this.BackColor = Color.Green;
+            }
+            else if (colorActual == Color.Blue)
+            {
+                this.BackColor = Color.Blue;
+            }
+            else if (colorActual == Color.Red)
+            {
+                this.BackColor = Color.Purple;
+            }
+            else if (colorActual == Color.Orange)
+            {
+                this.BackColor = Color.Black;
+            }
+            else if (colorActual == Color.Purple)
+            {
+                this.BackColor = Color.Blue;
+            }
+            else
+            {
+                this.BackColor = Color.Blue;
+            }
+            azulRequerido = int.Parse(lAzul.Text);
+            azulRequerido++;
+            lAzul.Text = azulRequerido.ToString();
+        }
+
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            string primerLugar, segundoLugar, tercerLugar;
+            int primerSumatoria, segundoSumatoria, tercerSumatoria;
+
+            if (amarilloRequerido >= rojoRequerido && amarilloRequerido >= azulRequerido)
+            {
+                primerLugar = "Amarillo";
+                primerSumatoria = amarilloRequerido;
+                if (rojoRequerido >= azulRequerido)
+                {
+                    segundoLugar = "Rojo";
+                    segundoSumatoria = rojoRequerido;
+                    tercerLugar = "Azul";
+                    tercerSumatoria = azulRequerido;
+                }
+                else
+                {
+                    segundoLugar = "Azul";
+                    segundoSumatoria = azulRequerido;
+                    tercerLugar = "Rojo";
+                    tercerSumatoria = rojoRequerido;
+                }
+            }
+            else if (rojoRequerido >= amarilloRequerido && rojoRequerido >= azulRequerido)
+            {
+                primerLugar = "Rojo";
+                primerSumatoria = rojoRequerido;
+                if (amarilloRequerido >= azulRequerido)
+                {
+                    segundoLugar = "Amarillo";
+                    segundoSumatoria = amarilloRequerido;
+                    tercerLugar = "Azul";
+                    tercerSumatoria = azulRequerido;
+                }
+                else
+                {
+                    segundoLugar = "Azul";
+                    segundoSumatoria = azulRequerido;
+                    tercerLugar = "Amarillo";
+                    tercerSumatoria = amarilloRequerido;
+                }
+            }
+            else
+            {
+                primerLugar = "Azul";
+                primerSumatoria = azulRequerido;
+                if (amarilloRequerido >= rojoRequerido)
+                {
+                    segundoLugar = "Amarillo";
+                    segundoSumatoria = amarilloRequerido;
+                    tercerLugar = "Rojo";
+                    tercerSumatoria = rojoRequerido;
+                }
+                else
+                {
+                    segundoLugar = "Rojo";
+                    segundoSumatoria = rojoRequerido;
+                    tercerLugar = "Amarillo";
+                    tercerSumatoria = amarilloRequerido;
+                }
+            }
+
+            string mensaje = $"1°. - Color \"{primerLugar}\": {primerSumatoria} veces\n" +
+                             $"2°. - Color \"{segundoLugar}\": {segundoSumatoria} veces\n" +
+                             $"3°. - Color \"{tercerLugar}\": {tercerSumatoria} veces";
+
+            MessageBox.Show("Ranking de Colores Primarios" + "\n" +mensaje);
+            Close();
+        }
+    }
+}
